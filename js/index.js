0060149523a878ee5af6182fffed89ad5f423b50
Olga Hutsulyuk <o.hutsulyuk@gmail.com>
2022-05-24 15:36:34 +0300

    add the first practical part

--- js/index.js
+++ js/index.js
@@ -2,7 +2,33 @@
 // console.log('====================================');
 // console.log(result);
 // console.log('====================================');
-const result = +prompt("Do you sleap 7 hour?"); // приходит всегда строка, + переобразует в число
+// const result = +prompt("Do you sleap 7 hour?"); // приходит всегда строка, + переобразует в число
+// console.log("====================================");
+// console.log(typeof result);
+// console.log("====================================");
+
+// 1)
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+let filmName = prompt("Один с последних просмотреных фильмов?");
+let filmName2 = prompt("Один с последних просмотреных фильмов?");
+
+let raitingFilm = +prompt("На сколько оценете эго?");
+let raitingFilm2 = +prompt("На сколько оценете эго?");
+
+// 2)
+const personalMovieDB = {
+  count: 2,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+personalMovieDB.count = numberOfFilms;
+personalMovieDB.movies.logan = raitingFilm;
+personalMovieDB.movies[filmName] = raitingFilm;
+personalMovieDB.movies[filmName2] = raitingFilm2;
+
 console.log("====================================");
-console.log(typeof result); 
+console.log(personalMovieDB);
 console.log("====================================");
