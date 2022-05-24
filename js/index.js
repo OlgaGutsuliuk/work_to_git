579edb614b6409a7f2fbd63ccd1770facc0c9843
Olga Hutsulyuk <o.hutsulyuk@gmail.com>
2022-05-24 14:00:42 +0300

    add first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// const result = confirm("Do you sleap all night?")
+// console.log('====================================');
+// console.log(result);
+// console.log('====================================');
+const result = +prompt("Do you sleap 7 hour?"); // приходит всегда строка, + переобразует в число
+console.log("====================================");
+console.log(typeof result); 
+console.log("====================================");
